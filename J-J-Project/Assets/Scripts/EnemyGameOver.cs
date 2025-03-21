98393422027067e96ba0438e3e77655b4cfe54f4
Planetthinker2 <146100370+Planetthinker2@users.noreply.github.com>
2025-03-21 00:11:19 -0500

    Frazier Commit
    Enemy spot player script added doing more to improve

---
+++ J-J-Project/Assets/Scripts/EnemyGameOver.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyGameOver : MonoBehaviour
+{
+
+    public string playerTag = "Player";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over! Player spotted!");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
